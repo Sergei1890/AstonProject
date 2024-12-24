bb718915205c078e668136b8d639038afe27f001
Sergei1890 <sergei1890@list.ru>
2024-12-24 20:29:43 +0300

    Added 2 fails

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        // Вызов методов для выполнения заданий
+        L3.task1();
+        L3.task2();
+        L3.task3();
+        L3.task4();
+        L3.task5();
+        L3.task6();
+        L3.task7();
+        L3.task8();
+        L3.task9();
+        L3.task10();
+        L3.task11();
+        L3.task12();
+        L3.task13();
+        L3.task14();
+    }
+
+}
+
