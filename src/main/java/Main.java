80f0c90286d0998f18cb954da738b0a2fe230ec2
Sergei1890 <sergei1890@list.ru>
2024-12-24 19:54:12 +0300

    gfs

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        // Вызов методов для выполнения заданий
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+        task9();
+        task10();
+        task11();
+        task12();
+        task13();
+        task14();
+    }
+
+
+}
