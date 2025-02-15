813a100e8cbd4f9969ea808c8ec1c2efd334eb27
Sergei1890 <sergei1890@list.ru>
2025-02-15 17:13:16 +0300

    some

--- src/test/java/ru/pavlov/FactorialTest.java
+++ src/test/java/ru/pavlov/FactorialTest.java
@@ -1,18 +1,19 @@
 package ru.pavlov;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class FactorialTest {
+public class FactorialTest {
     private Factorial calculator = new Factorial();
 
     @Test
-    void testFactorialOfZero() {
+    public void testFactorialOfZero() {
         assertEquals(1, calculator.factorial(0));
     }
 
     @Test
-    void testFactorialOfNumber() {
+    public void testFactorialOfNumber() {
         assertEquals(1, calculator.factorial(1));
         assertEquals(2, calculator.factorial(2));
         assertEquals(6, calculator.factorial(3));
@@ -21,12 +22,12 @@ class FactorialTest {
 
     @Test
         //Для любого n > 9:
-    void testFactorialOfBigNumber() {
+    public void testFactorialOfBigNumber() {
         assertEquals(0, (calculator.factorial(10))%100);
     }
 
     @Test
-    void testNegativeNumber() {
+    public void testNegativeNumber() {
         assertThrows(IllegalArgumentException.class, () -> calculator.factorial(-1));
         assertThrows(IllegalArgumentException.class, () -> calculator.factorial(-2));
     }
