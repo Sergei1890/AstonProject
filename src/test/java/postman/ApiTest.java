f34ff1a4ee5970c2b8bc0b73d5a9f3167d5dd487
Sergei1890 <sergei1890@list.ru>
2025-02-28 01:11:38 +0300

    Commit1

---
+++ src/test/java/postman/ApiTest.java
@@ -0,0 +1,90 @@
+package postman;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class ApiTest {
+    @BeforeEach
+    public void setup() {
+        RestAssured.baseURI = "https://postman-echo.com";
+    }
+
+    @Test
+    public void testGetRequest() {
+        given()
+                .queryParam("foo1", "bar1")
+                .queryParam("foo2", "bar2")
+                .when()
+                .get("/get")
+                .then()
+                .statusCode(200)
+                .body("args.foo1", equalTo("bar1"))
+                .body("args.foo2", equalTo("bar2"));
+    }
+
+    @Test
+    public void testPostRawText() {
+        given()
+                .header("Content-Type", "text/plain")
+                .body("This is expected to be sent back as part of response body.")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("This is expected to be sent back as part of response body."));
+    }
+
+    @Test
+    public void testPostFormData() {
+        given()
+                .contentType("application/x-www-form-urlencoded")
+                .formParam("foo1", "bar1")
+                .formParam("foo2", "bar2")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("form.foo1", equalTo("bar1"))
+                .body("form.foo2", equalTo("bar2"));
+    }
+
+    @Test
+    public void testPutRequest() {
+        given()
+                .header("Content-Type", "text/plain")
+                .body("This is expected to be sent back as part of response body.")
+                .when()
+                .put("/put")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("This is expected to be sent back as part of response body."));
+    }
+
+    @Test
+    public void testPatchRequest() {
+        given()
+                .header("Content-Type", "text/plain")
+                .body("This is expected to be sent back as part of response body.")
+                .when()
+                .patch("/patch")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("This is expected to be sent back as part of response body."));
+    }
+
+    @Test
+    public void testDelete() {
+        given()
+                .header("Content-Type", "text/plain")
+                .body("This is expected to be sent back as part of response body.")
+                .when()
+                .delete("/delete")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("This is expected to be sent back as part of response body."));
+    }
+}
